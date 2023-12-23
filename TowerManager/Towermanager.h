b15cdee418a90bf6e36faec54cee36e337ec8f39
udkzzz <1762618136@qq.com>
2023-12-23 16:10:29 +0800

    炮塔基类+三种炮塔+炮塔管理器
    第三次上传

---
+++ TowerManager/Towermanager.h
@@ -0,0 +1,22 @@
+#ifndef TOWERMANAGER_H
+#define TOWERMANAGER_H
+
+#include "cocos2d.h"
+#include "BasicDefensiveTower.h"
+#include <unordered_map>
+
+class TowerManager {
+public:
+    static TowerManager* getInstance();
+
+    void addTower(const std::string& towerName, BasicDefensiveTower* tower);
+    void removeTower(const std::string& towerName);
+    BasicDefensiveTower* getTower(const std::string& towerName);
+
+private:
+    TowerManager() {}
+    static TowerManager* instance;
+    std::unordered_map<std::string, BasicDefensiveTower*> towers;
+};
+
+#endif // TOWERMANAGER_H
