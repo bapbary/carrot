44bd5b8e3f90b9ca37bea2ec55a8e59a0b7d2e53
cyyyyyyyyy <152495317+cyy20040629@users.noreply.github.com>
2023-12-31 18:29:13 +0800

    Update GameObject.h

--- GameObject/GameObject.h
+++ GameObject/GameObject.h
@@ -7,27 +7,27 @@
 //地图终点、起点
 const cocos2d::Vec2 Destination[2] = { cocos2d::Vec2(736, 180),cocos2d::Vec2(890,269) };
 const cocos2d::Vec2 Origin[2] = { cocos2d::Vec2(383, 532),cocos2d::Vec2(994,531) };
-#define MONSTER 200
-#define CARROT 100
-#define LIGHTINGBULLET1 3
-#define LIGHTINGBULLET2 6
-#define LIGHTINGBULLET3 9
-#define SUNFLOWERBULLET1 10
-#define SUNFLOWERBULLET2 15
-#define SUNFLOWERBULLET3 20
-#define LEAFBULLET1 4
-#define LEAFBULLET2 8
-#define LEAFBULLET3 12
-#define OBSTACLE 300
-const float MonsterOneHealth = 100.f;
-const float MonsterTwoHealth = 200.f;
-const float MonsterThreeHealth = 300.0f;
-const float MonsterOneValue = 100.0f;
-const float MonsterTwoValue = 200.0f;
-const float MonsterThreeValue = 300.0f;
-const float MonsterOneSpeed = 50.0f;
-const float MonsterTwoSpeed = 65.0f;
-const float MonsterThreeSpeed = 55.f;
+const int  MONSTER = 200;
+const int  CARROT = 100;
+const int  LIGHTINGBULLET1 = 3;
+const int  LIGHTINGBULLET2 = 6;
+const int  LIGHTINGBULLET3 = 9;
+const int  SUNFLOWERBULLET1 = 10;
+const int  SUNFLOWERBULLET2 = 15;
+const int  SUNFLOWERBULLET3 = 20;
+const int  LEAFBULLET1 = 4;
+const int  LEAFBULLET2 = 8;
+const int  LEAFBULLET3 = 12;
+const int  OBSTACLE = 300;
+const int MonsterOneHealth = 100;
+const int MonsterTwoHealth = 200;
+const int MonsterThreeHealth = 300;
+const int MonsterOneValue = 100;
+const int MonsterTwoValue = 200;
+const int MonsterThreeValue = 300;
+const int MonsterOneSpeed = 50;
+const int MonsterTwoSpeed = 65;
+const int MonsterThreeSpeed = 55;
 class Carrot;
 class MonsterManager;
 class Obstacles;
@@ -57,7 +57,7 @@ public:
     void addObstaclesInMapOne();
     void addObstaclesInMapTwo();
     //更新血量
-    void updateHealthBar(cocos2d::ProgressTimer* healthBar,float initial,float current);
+    void updateHealthBar(cocos2d::ProgressTimer* healthBar,int initial,int current);
     //接触
     bool onContactBegin(cocos2d::PhysicsContact& contact);
     //object位置
@@ -77,8 +77,8 @@ public:
     void update(float dt);
     float speed;
     MonsterManager* monstermanager;
-    void hitMonster(cocos2d::Node* node, float num, float scale, char* filename);
-    void hitObstacle(cocos2d::Node* node, float num, float scale, char* filename);
+    void hitMonster(cocos2d::Node* node, int num, float scale, char* filename);
+    void hitObstacle(cocos2d::Node* node, int num, float scale, char* filename);
     static GameObject* instance;
     static GameObject* getInstance();
     std::vector<Obstacles*> getObstacles();
